cddd9e1cc159d43bab49307757a8f917c1698dc1
Seth Rubin <seth.h.rubin@gmail.com>
2016-10-16 16:54:01 -0400

    google id

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var ejs = require('ejs');
-var fs = require('fs');
 var io = require('socket.io')(http);
 
 var mongoose = require('mongoose');
@@ -10,32 +8,25 @@ mongoose.connect(mongodbUri);
 var Message = require('./models/message');
 
 app.set('port', process.env.PORT || 3000);
-const SERVER = process.env.SERVER_URL || 'localhost:' + app.get('port');
-
-app.get('/', function(req, res){
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  fs.readFile('index.html', 'utf-8', function(err, content) {
-    if (err) {
-      res.end('error occurred');
-      return;
-    }
-    var html = ejs.render(content, {server: SERVER});
-    res.end(html);
-  });
-});
 
 io.on('connection', function(socket){
   var room = socket.handshake.query.url;
+  var id = socket.handshake.query.id;
+  if (!room || !id) {
+    console.log('no room or id provided');
+    return;
+  }
   socket.join(room);
 
   console.log('a user connected to ' + room);
+  console.log('id: ' + id);
   socket.on('disconnect', function(){
     console.log('user disconnected from ' + room);
   });
 
   socket.on('chat message', function(obj){
     var message = new Message({
-      uid: '123',
+      uid: id,
       url: room,
       domain: room,
       username: obj.name,
